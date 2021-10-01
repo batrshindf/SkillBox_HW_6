398ebcf6081fcd58bc53d866df89b6415ec865cd
Денис Батршин <batrshindf@yandex.ru>
2021-10-01 04:06:44 +0300

    Добавьте файлы проекта.

---
+++ SkillBox_HW_6/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillBox_HW_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
